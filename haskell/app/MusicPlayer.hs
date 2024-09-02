3aefd638227bbd36d1c820e52e511f65d5410f75
Kaio <kaio.silva.carvalho@ccc.ufcg.edu.br>
2024-09-02 00:00:47 -0300

    Consertando caminho das músicas.

--- haskell/app/MusicPlayer.hs
+++ haskell/app/MusicPlayer.hs
@@ -4,13 +4,13 @@ import System.Process (callCommand)
 
 tocarMusica :: Int -> IO ()
 tocarMusica m = do
-    if  m == 1 then callCommand "cvlc --play-and-exit guitar-hero/songs/Even.mp3 > /dev/null 2>&1  &"
-    else if m == 2 then callCommand "cvlc --play-and-exit guitar-hero/songs/Paint.mp3 > /dev/null 2>&1  &"
-    else callCommand "cvlc --play-and-exit guitar-hero/songs/Bulls.mp3 > /dev/null 2>&1  &"
+    if  m == 1 then callCommand "cvlc --play-and-exit haskell/songs/Even.mp3 > /dev/null 2>&1  &"
+    else if m == 2 then callCommand "cvlc --play-and-exit haskell/songs/Paint.mp3 > /dev/null 2>&1  &"
+    else callCommand "cvlc --play-and-exit haskell/songs/Bulls.mp3 > /dev/null 2>&1  &"
     
 tocarMusicaMenu :: IO ()
 tocarMusicaMenu = do
-    callCommand "cvlc --loop guitar-hero/songs/Menu.mp3 > /dev/null 2>&1 &"
+    callCommand "cvlc --loop haskell/songs/Menu.mp3 > /dev/null 2>&1 &"
     
 pararMusica :: IO ()
 pararMusica = callCommand "pkill vlc"
